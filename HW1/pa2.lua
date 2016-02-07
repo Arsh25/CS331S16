948934989e1d3f8dd09e3ee29ccf35fd60d62cab
Arsh Chauhan <achauhan@alaska.edu>
2016-02-06 19:56:35 -0900

    pa2.lua: Added all required functions. Now runs with test program

---
+++ HW1/pa2.lua
@@ -0,0 +1,17 @@
+local pa2 = {}
+
+function pa2.concatLimit(str, num)
+	return str --Dummy return
+end
+	
+function pa2.filterTable(f, t)
+	finalTable = {}
+	return finalTable --Dummy return
+end
+	
+function pa2.collatzSeq(k)
+	return k --Dummy return
+end
+
+return pa2
+
