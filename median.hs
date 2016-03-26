561d0a326eb3eef8c7d91a33ca341cd05247b68d
Arsh Chauhan <achauhan@ua.alaska.edu>
2016-03-26 13:47:57 -0800

    Added skeletal median source files

---
+++ median.hs
@@ -0,0 +1,20 @@
+module Main where 
+import System.IO
+
+main = do
+    putStrLn ""
+    putStrLn "I will compute the median of the list."
+	--enteredList : Numlist
+
+--Numlist = do	
+	--putStrLn "Enter number (blank line to end): "
+	--input <- getLine 
+	--if num == "" 
+		--then return (inputList)
+		
+	--else do	
+		--enteredNums ++ [num]
+		--let num = input : Integer
+		--num : inputList
+		--nextNum <- Numlist
+
