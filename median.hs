25c9310a5260f3749b567354d2347b1ead386437
Arsh Chauhan <achauhan@ua.alaska.edu>
2016-03-26 16:58:36 -0800

    Something

--- median.hs
+++ median.hs
@@ -2,19 +2,16 @@ module Main where
 import System.IO
 
 main = do
-    putStrLn ""
-    putStrLn "I will compute the median of the list."
-	--enteredList : Numlist
+    putStrLn ("I will compute the median of the list.")
+    -- numlist
 
---Numlist = do	
-	--putStrLn "Enter number (blank line to end): "
-	--input <- getLine 
-	--if num == "" 
-		--then return (inputList)
-		
-	--else do	
-		--enteredNums ++ [num]
-		--let num = input : Integer
-		--num : inputList
-		--nextNum <- Numlist
-
+numlist = do	
+    -- putStrLn "Enter number (blank line to end):"
+    input <- getLine 
+    if input == "" 
+		then 
+		    return []
+	    else do
+		    let num = read input :: Int
+            nextNum <- numlist
+		    return (num : nextNum)
