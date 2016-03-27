b91c16c5c36f825cd4f3270bb862387e2f62572e
achauhan25 <achauhan@alaska.edu>
2016-03-27 05:30:29 +0000

    Everything runs

--- HW5/PA5.hs
+++ HW5/PA5.hs
@@ -1,5 +1,56 @@
 module PA5 where 
+import Data.List
 
-collazCountz n = div n 2 -- Dummy function 
+collatz num
+    | num == 0 = 0
+    | mod num 2 == 0 = div num 2
+    | otherwise = 3*num+1
+    
+collatzSeq num 
+    | num == 1 = 0
+    | collatz num == 1 = 1
+    | otherwise = 1 + collatzSeq (collatz num)
 
-findList 
+collatzCounts = map collatzSeq [1..]
+
+findList :: Eq a => [a] -> [a] -> Maybe Int
+findList list1 list2 = findIndex (isPrefixOf list1) (tails list2)
+
+search :: Eq a => [[a]] -> Integer
+search list 
+    | list == []  = 0
+    | length (head list) <= 1 = 0
+    | head (head list) == last (head list) = 1 + search (tail list)
+    | otherwise = search (tail list)
+
+list1 ## list2 = n where
+    transposedList = transpose [list1, list2]
+    n = search (transposedList)
+
+
+getTuplefirst (a,_) = a
+getTuplesecond (_,b) = b
+
+filterAB fn list1 list2 = anslist where 
+    pairedList = zip list1 list2
+    trueList = filter (fn.fst) pairedList
+    anslist = getTuplesecond (unzip trueList)
+    
+    
+    
+parts [] = ([], [])
+parts [x] = ([x],[])
+parts (x:y:xs) = (x:xp, y:yp ) where (xp, yp) = parts xs
+
+
+sumEvenOdd list = tuple where
+    tupleParts = parts list
+    evenSum = foldr (+) 0 (getTuplefirst tupleParts)
+    oddSum = foldr (+) 0 (getTuplesecond tupleParts)
+    tuple = (evenSum, oddSum)
+    
+    
+
+
+    
+    
