68cbac1a380df964d872d44af47e00fa15a5c2aa
achauhan25 <achauhan@alaska.edu>
2016-03-29 21:38:36 +0000

    Documentation

--- HW5/median.hs
+++ HW5/median.hs
@@ -3,17 +3,33 @@
 -- Source code for Assn5, Ex B
 -- 03/29/2015
 
-module Main where 
+-- Haskell module to ask the user for a list of numbers 
+-- and caluclate the median
+
+-- Invariants:
+--    Must input atleast 1 number. Program exists if median if given an empty list
+--    All input must be integers. Does not handle floating point 
 import System.IO
 import Data.List
+import Data.Char
+import Data.Int
 
 main = do
-    putStrLn "I will compute the median of the list."
+    putStrLn "Enter a list of numbers"
     ans <- median
     putStr "Median is :"
     hFlush stdout
     print (ans)
-
+    putStrLn "Do you want to run this program again? (yes/no) "
+    startOver <- getLine
+    if map toLower startOver == "yes"
+        then 
+            main
+        else do
+            putStrLn "Thank You for using this program."
+        
+-- Pre: Must enter atleast 1 number
+-- Post: Returns a list of Integers
 numlist = do	
     putStrLn "Enter number (blank line to end):"
     input <- getLine
@@ -24,12 +40,23 @@ numlist = do
             let num = read input :: Int
             nextNum <- numlist
             return (num:nextNum)
+
+--Pre: length of passed list >= 1
+--Post: Returns median of list
+--     Returns the mid element for even lists. 
+--     This meets the requirement for the assignment but is is mathematically correct  
 median = do
+    putStrLn "I will now compute the median"
     nums <- numlist
-    sortedNums <- listsort (nums)
-    len <- listlen (sortedNums)
-    mid <- mid (len)
-    return (sortedNums !! mid)
+    if length nums == 0
+        then
+            error ("Empty List: No median") -- Exit with error message
+        else do 
+            sortedNums <- listsort (nums)
+            len <- listlen (sortedNums)
+            mid <- mid (len)
+            return (sortedNums !! mid)
+
 
 listsort thelist = do
     return (sort thelist)
